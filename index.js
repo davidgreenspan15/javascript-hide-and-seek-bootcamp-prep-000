5695ab81100cc599a698a31985121f0a85a2af36
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-03-01 14:29:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  var ranks = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+}
+
+function deepestChild(){
+  var node = document.querySelector('#grand-node')
+  var deepestNode = node.children[0]
+  for (var i = 0; deepestNode.children[i]; i) {
+    deepestNode = deepestNode.children[0]
+  }
+  return deepestNode;
+
+
+}
